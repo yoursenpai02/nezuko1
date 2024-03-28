0c5ae0631e1bd95ab57ca7c4f94a2f6de5a7e06f
yoursenpai02 <159107953+yoursenpai02@users.noreply.github.com>
2024-03-28 16:24:39 +0530

    Update config.py
    Signed-off-by: yoursenpai02 <159107953+yoursenpai02@users.noreply.github.com>

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 
 BOT_TOKEN = getenv("BOT_TOKEN", None)
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "17000"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
@@ -18,8 +18,8 @@ START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+ZJ1TjUFG3llmNzRl")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Senpaibots02")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
 
